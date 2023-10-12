c8def7b74894384f05e173dfed009eb6aa5a34fe
teorth <tao@math.ucla.edu>
2023-10-12 14:33:32 -0700

    Implemented a proof that S_0(x)=1

--- test.lean
+++ test.lean
@@ -1,15 +1,22 @@
+import Mathlib.Data.Real.Basic
 import Mathlib.RingTheory.MvPolynomial.Basic
 import Mathlib.RingTheory.MvPolynomial.Symmetric
 
 open MvPolynomial
 
--- the k^th elementary symmetric polynomial in n real variables
+-- real_esymm n k is the k^th elementary symmetric polynomial in n real variables
+noncomputable def real_esymm (n : ℕ) (k : ℕ): MvPolynomial (Fin n) ℝ := 
+  esymm (Fin n) ℝ k
 
-def esymm (n : ℕ) (k : N): MvPolynomial (fin n) ℝ := 
-  MvPolynomial.esymm (fin n) ℝ k
+variable {n : ℕ}
+variable {x : Fin n → ℝ}
 
-#check fin 
-
+-- S_0(x) = 1
+example : eval x (real_esymm n 0) = 1 := by 
+  rw [real_esymm]  
+  rw [esymm_zero]
+  rw [<- C_1]
+  rw [eval_C]
 
 
 
