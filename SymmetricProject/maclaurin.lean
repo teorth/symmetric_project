8e145e01e4313d48abacb6a13b431d64086c4ec1
teorth <tao@math.ucla.edu>
2023-10-17 18:07:57 -0700

    Started file on Maclaurin

---
+++ SymmetricProject/maclaurin.lean
@@ -0,0 +1,9 @@
+import Mathlib.Algebra.BigOperators.Basic
+import Mathlib.Data.Finset.Fin
+import Mathlib.Data.Fintype.Fin
+import SymmetricProject.newton
+
+/-! Proof of the Maclaurin inequality - monotone decreasing nature of s_k^{1/k} when the s_k are non-negative. -/
+
+open Finset
+
