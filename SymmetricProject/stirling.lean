66137b0645baf6eca9c24a2349961fc2e3e9b68d
teorth <tao@math.ucla.edu>
2023-10-21 18:41:50 -0700

    variants of Stirling

--- SymmetricProject/stirling.lean
+++ SymmetricProject/stirling.lean
@@ -16,6 +16,7 @@ open Nat
 open Finset
 open BigOperators
 
+/-- n! is upper bounded by n^n -/
 lemma factorial_le {n : ℕ} : n ! ≤ n^n := by
   induction' n with m hm
   . simp
@@ -25,6 +26,7 @@ lemma factorial_le {n : ℕ} : n ! ≤ n^n := by
   gcongr
   linarith
 
+/-- n! is lower bounded by n^n/e^n -/
 lemma factorial_ge {n : ℕ} : n ! ≥ n^n / exp n := by
   have h := calc
     exp n ≥ ∑ k in range (n+1), (n:ℝ)^k / k ! := by
@@ -51,6 +53,7 @@ lemma factorial_ge {n : ℕ} : n ! ≥ n^n / exp n := by
   intro a b hab
   gcongr
 
+/-- choose n k can be written as a product --/
 lemma choose_eq {n : ℕ} {k : ℕ} (h : k ≤ n) : choose n k = (∏ j in range k, (1 - (j:ℝ)/n)) * n^k / k ! := by
   have : choose n k = (descFactorial n k : ℝ) / k ! := by
     rw [descFactorial_eq_factorial_mul_choose]
@@ -74,7 +77,7 @@ lemma choose_eq {n : ℕ} {k : ℕ} (h : k ≤ n) : choose n k = (∏ j in range
   rw [Nat.sub_add_cancel]
   linarith
 
-
+/-- choose n k is bounded above by n^k/k! -/
 lemma choose_le {n : ℕ} {k : ℕ} (h : k ≤ n) : choose n k ≤ (n:ℝ)^k / k ! := by
   have h1 : (n:ℝ)^k/k ! = (∏ j in range k, (1:ℝ)) * n^k / (k !:ℝ) := by rw [prod_const_one]; norm_cast; simp
   rw [h1, choose_eq h]
@@ -87,12 +90,13 @@ lemma choose_le {n : ℕ} {k : ℕ} (h : k ≤ n) : choose n k ≤ (n:ℝ)^k / k
     norm_cast; linarith [hi, h]
   simp at hi; field_simp; positivity
 
+/-- choose n k is bounded below by n^k/k^k -/
 lemma choose_ge {n : ℕ} {k : ℕ} (h : k ≤ n) : choose n k ≥ (n:ℝ)^k / (k:ℝ)^k := by
   suffices : (k:ℝ)^k * choose n k ≥  (choose k k) * n^k
   . simp at this
     rw [ge_iff_le, div_le_iff, mul_comm]
     . exact this
-    induction' k with m hm
+    induction' k with m _
     . simp
     positivity
 
@@ -108,8 +112,25 @@ lemma choose_ge {n : ℕ} {k : ℕ} (h : k ≤ n) : choose n k ≥ (n:ℝ)^k / (
   simp at hi; norm_cast; linarith
 
 
+lemma choose_le' {n : ℕ} {k : ℕ} (h : k ≤ n) (h2 : 0 < k): (choose n k : ℝ)^((1:ℝ) / k) ≤ (exp 1) * n / k := by
+  replace h2 : 0 < (k:ℝ) := by norm_cast
+  rw [<- rpow_le_rpow_iff _ _ h2, <- rpow_mul, div_rpow, mul_rpow, (show 1/(k:ℝ) * k = 1 by field_simp), exp_one_rpow]
+  . simp
+    apply (le_trans (choose_le h) _)
+    rw [div_le_div_iff]
+    . calc
+        (n:ℝ)^k * (k:ℝ)^k = (rexp k) * (n:ℝ)^k * ((k:ℝ)^k / (rexp k)) := by field_simp; ring
+        _ ≤ (rexp k) * (n:ℝ)^k * k ! := by
+          gcongr
+          rw [<-ge_iff_le, (show (k:ℝ)^k = (k^k :ℕ) by norm_cast)]
+          apply factorial_ge
+    all_goals {positivity}
+  all_goals { positivity }
 
-
-
-
-  sorry
+lemma choose_ge' {n : ℕ} {k : ℕ} (h : k ≤ n) (h2 : 0 < k) : (choose n k : ℝ)^((1:ℝ) / k) ≥  n / k := by
+  replace h2 : 0 < (k:ℝ) := by norm_cast
+  rw [ge_iff_le, <- rpow_le_rpow_iff _ _ h2, <- rpow_mul, div_rpow, (show 1/(k:ℝ) * k = 1 by field_simp)]
+  . simp
+    rw [<-ge_iff_le]
+    apply choose_ge
+  all_goals {positivity}
